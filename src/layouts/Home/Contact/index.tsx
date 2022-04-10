6270ca4f9d9029ec3a2d5d9655b664fed088c598
Luiz <doginuwu@gmail.com>
2022-04-10 13:15:48 -0300

    ✨ feat: add temporary toasts

--- src/layouts/Home/Contact/index.tsx
+++ src/layouts/Home/Contact/index.tsx
@@ -1,51 +1,58 @@
 import { Icon } from '@iconify/react';
-import React from 'react';
+import React, { FormEvent } from 'react';
+import toast from 'react-hot-toast';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 import Textarea from '../../../components/Textarea';
 
 import { Card, Cards, Container, Content, Footer, Form, Header } from './styles';
 
-const Contact: React.FC = () => (
-    <Container>
-        <Content>
-            <Header>
-                <h1>Entre em contato</h1>
-                <p>Fique a vontade para escolher o modo de contato, irei te responder o mais breve possível.</p>
-            </Header>
-            <Cards>
-                <Card>
-                    <Icon icon="bxs:phone" />
-                    +55 (43) 9 8492-2480
-                </Card>
-                <Card active>
-                    <Icon icon="ic:baseline-email" />
-                    luizgotardo01@gmail.com
-                </Card>
-                <Card>
-                    <Icon icon="carbon:location-filled" />
-                    Cambé, Parque Manela
-                </Card>
-            </Cards>
-            <Footer>
-                <a href="https://github.com/DoginUwU" target="_blank" rel="noreferrer">
-                    <Icon icon="akar-icons:github-fill" />
-                </a>
-                <a href="https://www.linkedin.com/in/luiz-gotardo/" target="_blank" rel="noreferrer">
-                    <Icon icon="akar-icons:linkedin-fill" />
-                </a>
-                <a href="https://discord.com/users/656129876092518400" target="_blank" rel="noreferrer">
-                    <Icon icon="akar-icons:discord-fill" />
-                </a>
-            </Footer>
-        </Content>
-        <Form>
-            <Input label="Seu nome" placeholder="Seu nome" />
-            <Input label="Email" placeholder="Email" />
-            <Textarea label="Mensagem" placeholder="Mensagem" />
-            <Button type="submit">Enviar</Button>
-        </Form>
-    </Container>
-);
+const Contact: React.FC = () => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        toast.error('Indisponível no momento.');
+    };
 
+    return (
+        <Container>
+            <Content>
+                <Header>
+                    <h1>Entre em contato</h1>
+                    <p>Fique a vontade para escolher o modo de contato, irei te responder o mais breve possível.</p>
+                </Header>
+                <Cards>
+                    <Card>
+                        <Icon icon="bxs:phone" />
+                        +55 (43) 9 8492-2480
+                    </Card>
+                    <Card active>
+                        <Icon icon="ic:baseline-email" />
+                        luizgotardo01@gmail.com
+                    </Card>
+                    <Card>
+                        <Icon icon="carbon:location-filled" />
+                        Cambé, Parque Manela
+                    </Card>
+                </Cards>
+                <Footer>
+                    <a href="https://github.com/DoginUwU" target="_blank" rel="noreferrer">
+                        <Icon icon="akar-icons:github-fill" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/luiz-gotardo/" target="_blank" rel="noreferrer">
+                        <Icon icon="akar-icons:linkedin-fill" />
+                    </a>
+                    <a href="https://discord.com/users/656129876092518400" target="_blank" rel="noreferrer">
+                        <Icon icon="akar-icons:discord-fill" />
+                    </a>
+                </Footer>
+            </Content>
+            <Form onSubmit={handleSubmit}>
+                <Input label="Seu nome" placeholder="Seu nome" />
+                <Input label="Email" placeholder="Email" />
+                <Textarea label="Mensagem" placeholder="Mensagem" />
+                <Button type="submit">Enviar</Button>
+            </Form>
+        </Container>
+    );
+};
 export default Contact;
--- src/layouts/Home/Projects/index.tsx
+++ src/layouts/Home/Projects/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 import { useTheme } from 'styled-components';
 import Button from '../../../components/Button';
 import Carousel from '../../../components/Carousel';
@@ -10,13 +11,21 @@ const Projects: React.FC = () => {
     const { projects } = useProjects();
     const theme = useTheme();
 
+    const handleViewAllProjects = (): void => {
+        toast.error('Indisponível no momento.');
+    };
+
     return (
         <Container>
             <Header>
                 <h1>Projetos</h1>
                 <div>
                     <p>Todos meus projetos estão aqui e no github, que tal dar uma olhada?</p>
-                    <Button color={theme.colors.white} textColor={theme.colors.semantic.blue}>
+                    <Button
+                        color={theme.colors.white}
+                        textColor={theme.colors.semantic.blue}
+                        onClick={handleViewAllProjects}
+                    >
                         Ver todos
                     </Button>
                 </div>
