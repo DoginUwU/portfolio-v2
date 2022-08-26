cd5e9febe62e428c701e3e2c721e834ae1ce65aa
Luiz Eduardo <doginuwu@gmail.com>
2022-08-26 09:55:09 -0300

    ✨ feat: add footer ambient svg

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,8 @@ import { Container } from './styles';
 
 const Footer: React.FC = () => (
     <Container>
-        <img src="assets/logo.svg" alt="My logo" />
+        <img src="/assets/logo.svg" alt="My logo" />
+        <img src="/assets/ambient.svg" className="ambient" alt="Ambient" />
     </Container>
 );
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,14 +1,29 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+    position: relative;
     width: 100%;
     justify-content: center;
     align-items: center;
     display: flex;
-    padding-bottom: 50px;
+    padding-bottom: 100px;
+    padding-top: 50px;
 
     & img {
         width: 150px;
+    }
+
+    & .ambient {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        padding: 0 100px;
+        opacity: 0.4;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+            display: none;
+        }
     }
 `;
 
